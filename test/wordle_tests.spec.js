eab9294219008911350a309fb12960e6875a2666
Mats Hemå <89969770+mhema42@users.noreply.github.com>
2022-03-16 22:05:00 +0100

    add 1 more test and change html-name to index.html and also on testfile

--- test/wordle_tests.spec.js
+++ test/wordle_tests.spec.js
@@ -61,6 +61,26 @@ function array_check (wordle_array, guess_array) {
   return result;
 };
 
+/* 
+in the following tests we test 
+- if the correct word and the guessed word has the same length
+- if the guessed word is correct
+- if letters in guessed word is "correct", "missplaced" or "incorrect"
+- if a "correct" letter is entered twice or more in guessed word (and the correct word only contains one version of the letter), 
+  the "missplaced" letter(s) should be "incorrect"
+- if a letter is entered twice in both words, and one is on correct place and the other is missplaced, they should be marked accordingly 
+- if a "missplaced" letter is entered twice or more in guessed word (and the correct word only contains one version of the letter), 
+  the first version of the letter should be marked "missplaced" and the following "incorrect"
+*/
+
+// test if words has different length
+test("wordle word and guessed word has different amount of letters", async () => {
+  const wordle_word = "abcde";
+  const guess_word = "abcdef";
+  const data = await word_check(wordle_word, guess_word);
+  expect(data).toBe('Your guess must contain 5 letters');
+});
+
 // test if guessed word is correct
 test("single correct letter", async () => {
   const wordle_word = "a";
@@ -108,10 +128,12 @@ test("two correct letters, two missplaced letters and one incorrect letter", asy
   expect(data).toBe('[ a, correct, c, missplaced, b, missplaced, b, incorrect, e, correct]');
 });
 
-// test if words has different length
-test("wordle word and guessed word has different amount of letters", async () => {
+// test if letters in guessed word are correct, missplaced or incorrect
+test("two correct letters, one missplaced letter and two incorrect letters", async () => {
   const wordle_word = "abcde";
-  const guess_word = "abcdef";
+  const guess_word = "addbe";
   const data = await word_check(wordle_word, guess_word);
-  expect(data).toBe('Your guess must contain 5 letters');
+  expect(data).toBe('[ a, correct, d, missplaced, d, incorrect, b, missplaced, e, correct]');
 });
+
+
